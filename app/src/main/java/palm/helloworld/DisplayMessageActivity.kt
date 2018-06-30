0152265a416a9de6d628264d70b5d42542a9e0b9
Craig Palmer <palmer.craig1@gmail.com>
2018-06-29 17:53:04 -0700

    My first simple Android App, initial commit

---
+++ app/src/main/java/palm/helloworld/DisplayMessageActivity.kt
@@ -0,0 +1,21 @@
+package palm.helloworld
+
+import android.app.Activity
+import android.os.Bundle
+import android.widget.TextView
+
+class DisplayMessageActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+
+        // Get the intent that started this activity and extract the string
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+
+        // capture the layout's text view and set the string as its text
+        val textView = findViewById<TextView>(R.id.textView).apply {
+            text = message
+        }
+    }
+}
